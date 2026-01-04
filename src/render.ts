97965ca859c6c2700eb6e5532974c0768025d61f
Hdr2804 <82568857+hdr2804@users.noreply.github.com>
2026-01-05 00:49:41 +0700

    Update and rename render.js to render.ts

---
+++ src/render.ts
@@ -0,0 +1,23 @@
+export function render(b, p) {
+  const z = 20
+  let r = ""
+
+  b.forEach((row, y) =>
+    row.forEach((c, x) => {
+      r += `<rect x="${x*z}" y="${y*z}" width="${z}" height="${z}"
+        fill="${c ? "#00ffcc" : "#111"}" stroke="#333"/>`
+    })
+  )
+
+  p.shape.forEach((row, y) =>
+    row.forEach((c, x) => {
+      if (c)
+        r += `<rect x="${(p.x+x)*z}" y="${(p.y+y)*z}"
+        width="${z}" height="${z}" fill="#ffcc00"/>`
+    })
+  )
+
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="200" height="400">
+    ${r}
+  </svg>`
+}
