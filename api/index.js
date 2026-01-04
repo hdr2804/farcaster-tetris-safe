fadf6a9eb68e225aa3d6b5e3a97b3a80a99e652a
Hdr2804 <82568857+hdr2804@users.noreply.github.com>
2026-01-05 00:40:25 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
-const { serve } = require("@hono/node-server")
+const { createServer } = require("@hono/node-server")
 const { app } = require("../src/index")
 
-module.exports = serve(app)
+module.exports = createServer({
+  fetch: app.fetch
+})
